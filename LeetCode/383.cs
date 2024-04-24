ba110cac1556406f4c9c348b05753d865b1adb1f
dannielzanoni <danielzanoni2001@gmail.com>
2024-04-24 14:02:10 -0300

    Added lc 383

---
+++ LeetCode/383.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _383
+    {
+        public void Execute()
+        {
+            string ransomNote = "aab";
+            string magazine = "baa";
+            bool result = CanConstruct(ransomNote, magazine);
+
+            bool CanConstruct(string ransomNote, string magazine)
+            {
+                Dictionary<char, int> magazineCount = new Dictionary<char, int>();
+
+                foreach (char letter in magazine)
+                {
+                    if (!magazineCount.ContainsKey(letter))
+                        magazineCount[letter] = 0;
+                    magazineCount[letter]++;
+                }
+                foreach (char letter in ransomNote)
+                {
+                    if (!magazineCount.ContainsKey(letter) || magazineCount[letter] == 0)
+                        return false;
+                    magazineCount[letter]--;
+                }
+
+                return true;
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main(string[] args)
     {   
-        _1342___Number_of_Steps num = new _1342___Number_of_Steps();
-        num.Execute();
+        _383 Ransom = new _383();
+        Ransom.Execute();
     }
 }
